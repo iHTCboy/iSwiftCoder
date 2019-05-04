76f6018c7a302702036ffa7747c28c23ae41829b
iHTCboy <ihetiancong@gmail.com>
2019-05-04 12:05:25 +0800

    init project

---
+++ iSwiftCoder/iSwiftCoder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iSwiftCoder
+//
+//  Created by HTC on 2019/5/4.
+//  Copyright © 2019 HTC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
